ea88e6f77765c0288fc35353fa1d77c5e7eb43fb
Marvin <marvin.xin@sjsu.edu>
2023-03-15 04:41:54 -0700

    First commit of palin.py

---
+++ palin.py
@@ -0,0 +1,12 @@
+"""
+Marvin Xin
+2023 - 03 - 15
+"""
+
+def palindrome(input):
+    print(input)
+    reverse = input[::-1]
+    if reverse == input:
+        return 1
+    elif reverse == input:
+        return 0
