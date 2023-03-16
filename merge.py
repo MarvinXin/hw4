e141770c23a2804821e1a686a7645e229ff758e3
Marvin <marvin.xin@sjsu.edu>
2023-03-15 22:18:12 -0700

    completed and tested merge.py

--- merge.py
+++ merge.py
@@ -3,6 +3,19 @@ Marvin Xin
 2023 - 03 - 15
 """
 
-def merge(input):
-    print(input)
+def merge(input1, input2):
+    print(sorted(input1 + input2))
+    return;
+    
+
+
+"""
+Testing to see if code runs
+
+list1 = [1,3,5,7]
+list2 = [2,9,12]
+
+result = merge(list1, list2)
+"""
+
     
