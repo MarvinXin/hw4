db306832340bea72b121386261d18d323040d133
Marvin Xin <marvin.xin@sjsu.edu>
2023-03-17 02:59:49 -0700

    Commit #6

--- sort.py
+++ sort.py
@@ -1,10 +1,11 @@
 def sort_dictionary(input):
     x = []
     sorted_list = sorted(input, key = lambda x: input[x][1])
+    for name in print(f'({name}, {input[name][0]})'):
     #for name in sorted(input, key = lambda x: input[x][1]):
        #print(f'({name}, {input[name][0]})')
        #x.append((name,input[name][0]))
-    return sorted_list
+        return sorted_list
        
 
 
