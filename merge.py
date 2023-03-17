14eae12f36d6fd30255647a1478cfba4712e1ee9
Marvin Xin <marvin.xin@sjsu.edu>
2023-03-17 02:07:40 -0700

    Commit merge.py commit 2

--- merge.py
+++ merge.py
@@ -5,17 +5,12 @@ Marvin Xin
 
 def merge(input1, input2):
     print(sorted(input1 + input2))
-    return;
+    return 
     
 
 
-"""
-Testing to see if code runs
 
-list1 = [1,3,5,7]
-list2 = [2,9,12]
 
-result = merge(list1, list2)
-"""
+
 
     
