c3beb1ee190de9dd1b02b71ab36eaac6c9e083db
Marvin Xin <marvin.xin@sjsu.edu>
2023-03-17 02:32:50 -0700

    Commit 3 sort.py

--- sort.py
+++ sort.py
@@ -6,11 +6,3 @@ def sort_dictionary(input):
        
 
 
-"""
-#My code tester:
-input_list={"Tom" : (5464512, 24) , "Sara" : (5446987, 32) , "Mary" : (1557896, 20)}
-#result = []
-result = sort_dictionary(input_list)
-print(result)
-"""
-
