9e807f1e0b8f10a44effa91daa9853fbb1f94265
Marvin Xin <marvin.xin@sjsu.edu>
2023-03-19 03:40:38 -0700

    Final sort.py

--- sort.py
+++ sort.py
@@ -3,11 +3,12 @@ def sort_dictionary(input):
     for name in sorted(input, key = lambda x: input[x][1]):
        x.append((name,input[name][0]))
 
-    return(x)  
+    return x 
 
 
     
-
+input_list={"Tom" : (5464512, 24) , "Sara" : (5446987, 32) , "Mary" : (1557896, 20)}
+result = sort_dictionary(input_list)
 
 
 
