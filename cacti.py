2e211fa75f908e675eaf26ad4e329ff885602d12
Marvin Xin <marvin.xin@sjsu.edu>
2023-03-20 03:44:18 -0700

    Created cacti.py

---
+++ cacti.py
@@ -0,0 +1,21 @@
+from cacti import cacti_number
+
+
+def plot ():
+    return main
+
+def main():
+    plot = [ [1, 0, 1, 0, 1]
+            [0, 0, 0, 0, 0]
+            [1, 0, 0, 0, 0] ]
+print(cacti_number(plot))
+
+#Test two
+"""
+from calc import calculator
+def main():
+    plot = [ [0, 1, 0, 0, 0, 0]
+        [0, 0, 0, 1, 0, 0]
+        [1, 0, 1, 0, 0, 1] ]
+    print(cacti_number(plot))
+"""
