139124610af2747128c26653c2d202036e353f58
Marvin Xin <marvin.xin@sjsu.edu>
2023-03-20 03:37:33 -0700

    First commit of log.py

---
+++ log.py
@@ -0,0 +1,19 @@
+import time
+#from log import timestamp
+
+def timestamp(func):
+    def inner():
+        print(time.ctime())
+        func()
+    return inner
+
+"""
+@timestamp
+def hi():
+    print('hi')
+
+def main():
+    hi()
+
+main()
+"""
